d949bfcf11dff9ebafd72f4c9be758f77af9523e
Renu verma <64033850+vermarenu349@users.noreply.github.com>
2022-04-06 18:44:44 +0530

    Add files via upload

--- day24-assignment/step4-featured-routes/src/app/welcome.component.ts
+++ day24-assignment/step4-featured-routes/src/app/welcome.component.ts
@@ -4,17 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-welcome',
   template: `
   <div class="container">
-  <a [routerLink]="['']">Home</a> <br>
   <a [routerLink]="['heroes']">Hero List</a><br>
-  <!--<a [routerLink]="['hero']"> Hero</a> <br>
- <a [routerLink]="['addhero']">Add Hero</a> |
-  <a [routerLink]="['edithero']">Edit Hero</a> |
-   |
-  <a [routerLink]="['movie']">Movie</a> |-->
   <a [routerLink]="['movies']">Movie List</a>
-  <!--<a [routerLink]="['addmovie']">Add Movie</a> |
-  <a [routerLink]="['editmovie']">Edit Movie</a>
-  -->
   </div>
   `,
   styles: []
